162f03de077ab1f38b676f8a26774dd7200f6d7b
NSW5 <noahwilliams389@riversideunified.org>
2023-12-19 11:54:13 -0800

    All Code Is Working

--- Final Fall Semester/Assets/Scripts/GameManager.cs
+++ Final Fall Semester/Assets/Scripts/GameManager.cs
@@ -9,28 +9,42 @@ public class GameManager : MonoBehaviour
     public bool isGameActive = false;
 
     public Button replayButton;
+    public Button startbutton;
+    public GameObject coinText;
 
     // Start is called before the first frame update
     void Start()
     {
         replayButton.gameObject.SetActive(false);
+        startbutton.gameObject.SetActive(true);
+        coinText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void GameOver()
     {
         isGameActive = false;
         replayButton.gameObject.SetActive(true);
-        
+
     }
 
     public void PressReplayButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void PressStartButton()
+    {
+        isGameActive = true;
+        coinText.SetActive(true);
+        startbutton.gameObject.SetActive(false);
+        GameObject.Find("Spawn Manager").GetComponent<SpawnManager>().SpawnCollectibleObject();
+        GameObject.Find("Spawn Manager").GetComponent<SpawnManager>().StartCreatingZombies();
+    }    
+
 }
